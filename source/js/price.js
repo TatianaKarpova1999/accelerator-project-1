3c5cb940d6530922795b7981d76e1393a6e3f6f5
Татьяна <tanya.karpova.ry@gmail.com>
2024-07-17 12:37:30 +0300

    Дорабатывает интерактив блока price

--- source/js/price.js
+++ source/js/price.js
@@ -15,13 +15,21 @@ function clickHandlerPrice(evt) {
   });
   evt.target.classList.add('price__tab--current');
 
-  if (evt.target.innerHTML === '6&nbsp;месяцев') {
-    priceTrainer.innerHTML = '30&nbsp;000';
-    priceDay.innerHTML = '10&nbsp;200';
-    priceFullDay.innerHTML = '16&nbsp;200';
-  } else if (evt.target.innerHTML === '12&nbsp;месяцев') {
-    priceTrainer.innerHTML = '60&nbsp;000';
-    priceDay.innerHTML = '20&nbsp;400';
-    priceFullDay.innerHTML = '32&nbsp;400';
+  switch (evt.target.innerHTML) {
+    case '1&nbsp;месяц':
+      priceTrainer.innerHTML = '5&nbsp;000';
+      priceDay.innerHTML = '1&nbsp;700';
+      priceFullDay.innerHTML = '2&nbsp;700';
+      break;
+    case '6&nbsp;месяцев':
+      priceTrainer.innerHTML = '30&nbsp;000';
+      priceDay.innerHTML = '10&nbsp;200';
+      priceFullDay.innerHTML = '16&nbsp;200';
+      break;
+    case '12&nbsp;месяцев':
+      priceTrainer.innerHTML = '60&nbsp;000';
+      priceDay.innerHTML = '20&nbsp;400';
+      priceFullDay.innerHTML = '32&nbsp;400';
+      break;
   }
 }
